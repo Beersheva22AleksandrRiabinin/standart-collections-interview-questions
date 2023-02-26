491dc02eff88b916618803602ee7832aee3bd32b
AleksandrRiabinin <alexr2141@gmail.com>
2023-02-26 20:49:58 +0200

    hw  #34

--- src/telran/structure/MultiCountersClass.java
+++ src/telran/structure/MultiCountersClass.java
@@ -15,37 +15,45 @@ public class MultiCountersClass implements MultiCounters {
 		int res = 1;
 		if (!hashmap.containsKey(item)) {
 			hashmap.put(item, 1);
-			if (treemap.isEmpty()) {
-				HashSet<Object> set = new HashSet<>();
-				set.add(item);
-				treemap.put(1, set);
-			} else {
-				HashSet<Object> set;
-				if (treemap.get(1) == null) {
-					set = new HashSet<>();
-				} else {
-					set = treemap.get(1);
-				}
-				set.add(item);
-				treemap.put(1, set);
-			}
+			putFirstItemOccurrence(item);
 		} else {
 			int incrementValue = hashmap.get(item) + 1;
 			hashmap.put(item, incrementValue);
 			res = incrementValue;
-			if (treemap.get(incrementValue) == null) {
-				HashSet<Object> set = new HashSet<>();
-				treemap.put(incrementValue, set);
-			}
-			HashSet<Object> set = treemap.get(incrementValue);
-			set.add(item);
-			treemap.put(incrementValue, set);
-			treemap.get(incrementValue - 1).remove(item);
-			if (treemap.get(incrementValue - 1).isEmpty()) {
-				treemap.remove(incrementValue - 1);
-			}
+			setFillingAndUpdating(item, incrementValue);
 		}
 		return res;
+	}
+
+	private void setFillingAndUpdating(Object item, int incrementValue) {
+		if (treemap.get(incrementValue) == null) {
+			HashSet<Object> set = new HashSet<>();
+			treemap.put(incrementValue, set);
+		}
+		HashSet<Object> set = treemap.get(incrementValue);
+		set.add(item);
+		treemap.put(incrementValue, set);
+		treemap.get(incrementValue - 1).remove(item);
+		if (treemap.get(incrementValue - 1).isEmpty()) {
+			treemap.remove(incrementValue - 1);
+		}
+	}
+
+	private void putFirstItemOccurrence(Object item) {
+		if (treemap.isEmpty()) {
+			HashSet<Object> set = new HashSet<>();
+			set.add(item);
+			treemap.put(1, set);
+		} else {
+			HashSet<Object> set;
+			if (treemap.get(1) == null) {
+				set = new HashSet<>();
+			} else {
+				set = treemap.get(1);
+			}
+			set.add(item);
+			treemap.put(1, set);
+		}
 	}
 
 	@Override
