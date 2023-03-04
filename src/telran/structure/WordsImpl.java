a8322f8e261336996e01b57d097c37849fc57861
AleksandrRiabinin <alexr2141@gmail.com>
2023-03-04 21:45:42 +0200

    cw words tree

--- src/telran/structure/WordsImpl.java
+++ src/telran/structure/WordsImpl.java
@@ -3,63 +3,22 @@ package telran.structure;
 import java.util.*;
 
 public class WordsImpl implements Words {
-	
-	HashSet<String> contains = new HashSet<>();
-	HashMap<String, Set<String>> words = new HashMap<>();
-
+	private TreeSet<String> set = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
 	@Override
 	public boolean addWord(String word) {
-		boolean res;
-		if (contains.contains(word.toLowerCase())) {
-			res = false;
-		} else {
-			res = true;
-			contains.add(word.toLowerCase());
-			String firstSymbol = word.substring(0, 1).toLowerCase();
-			Set<String> set;
-			if (words.containsKey(firstSymbol)) {
-				set = words.get(firstSymbol);
-			} else {
-				set = new HashSet<>();
-			}
-			set.add(word);
-			words.put(firstSymbol, set);
-		}
 		
-		return res;
+		return set.add(word);
 	}
 
 	@Override
-	public List<String> getWordsByPrefix(String pref) {
-		String prefix = pref.toLowerCase();
-		String firstSymbol = prefix.substring(0, 1);
-		if (!words.containsKey(firstSymbol)) {
-			return null;
-		}
-		Set<String> set = words.get(firstSymbol);
-		String[] ar = new String[set.size()];
-		ar = set.toArray(ar);
-		Arrays.sort(ar);
-		Comparator<String> comp = (o1, o2) -> {
-			return o1.substring(0, prefix.length()).compareTo(o2);
-		};
-		int anyMatchIndex = Arrays.binarySearch(ar, prefix, comp);
-		if (anyMatchIndex < 0) {
-			return null;
-		}		
-		int firstIndex = anyMatchIndex;
-		int lastIndex = anyMatchIndex;
-		while (firstIndex > - 1 && ar[firstIndex].toLowerCase().startsWith(prefix)) {
-			firstIndex--;
-		}
-		while (lastIndex < ar.length && ar[lastIndex].toLowerCase().startsWith(prefix)) {
-			lastIndex++;
-		}
-		List<String> res = new ArrayList<>();
-		for (int i = firstIndex + 1; i < lastIndex; i++) {
-			res.add(ar[i]);
-		}
-		return res;
+	public List<String> getWordsByPrefix(String prefix) {
+		return new ArrayList<>(set.subSet(prefix, getPrefixLimit(prefix)));
+	}
+	private String getPrefixLimit(String prefix) {
+		char lastChar = prefix.charAt(prefix.length() - 1);
+		char limitChar = (char) (lastChar + 1);
+		return prefix.substring(0, prefix.length() - 1) + limitChar;
+
 	}
 
 }
--- src/telran/test/WordsTest.java
+++ src/telran/test/WordsTest.java
@@ -1,45 +1,41 @@
 package telran.test;
 
 import static org.junit.Assert.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import telran.structure.Words;
 import telran.structure.WordsImpl;
+
 
 class WordsTest {
 
-	WordsImpl wrds = new WordsImpl();
+	String words[]= {"abcdef","ab123","aaa","ab","ablmn","abbbb",
+			"a", "ABd","bbb", "B12", "*/"};
+	String wordsStartB[] = {"B12", "bbb"};
+	String wordsStartAB[] = {"ab","ab123","abbbb","abcdef","ABd","ablmn"};
+	String wordsStartABC[] = {"abcdef"};
+	String wordsStartAsteric[] = {"*/"};
+	Words elasticSearch;
+		@BeforeEach
+		void setUp() throws Exception {
+			elasticSearch = new WordsImpl();
+			for(String word: words) {
+				elasticSearch.addWord(word);
+			}
+				
+				
+		}
 
-	@Test
-	void test() {
-		assertTrue(wrds.addWord("Ward1"));
-		assertFalse(wrds.addWord("ward1"));
-		assertFalse(wrds.addWord("Ward1"));
-		assertFalse(wrds.addWord("waRd1"));
+		@Test
+		void test() {
+			assertArrayEquals(wordsStartABC,
+					elasticSearch.getWordsByPrefix("abc").toArray(String[]::new));
+			assertArrayEquals(wordsStartB, elasticSearch.getWordsByPrefix("B").toArray(String[]::new));
+			assertArrayEquals(wordsStartAB, elasticSearch.getWordsByPrefix("ab").toArray(String[]::new));
+			assertArrayEquals(wordsStartAsteric, elasticSearch.getWordsByPrefix("*").toArray(String[]::new));
+			
+		}
 
-		wrds.addWord("word2");
-		wrds.addWord("word3");
-		wrds.addWord("ww");
-		wrds.addWord("www");
-
-		String[] exp = { "word2", "word3" };
-		assertArrayEquals(exp, wrds.getWordsByPrefix("Wo").toArray());
-		
-		String[] exp2 = { "Ward1", "word2", "word3", "ww", "www"};
-		assertArrayEquals(exp2, wrds.getWordsByPrefix("w").toArray());
-		
-		String[] exp3 = {"ww", "www"};
-		assertArrayEquals(exp3, wrds.getWordsByPrefix("ww").toArray());
-		
-		wrds.addWord("Step");
-		wrds.addWord("stone");
-		wrds.addWord("stop");
-		String[] exp4 = {"Step", "stone", "stop"};
-		assertArrayEquals(exp4, wrds.getWordsByPrefix("sT").toArray());		
-		
-		assertNull(wrds.getWordsByPrefix("qq"));
-		assertNull(wrds.getWordsByPrefix("wq"));
-	}
 
 }
